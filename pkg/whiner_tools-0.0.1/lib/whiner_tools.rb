49679e7820d3897050e56f95ca49f3f7d9a26ef5
jcastagna <jeremie.castagna@gmail.com>
2009-08-03 14:26:04 -0700

    Removing pkg directory

--- pkg/whiner_tools-0.0.1/lib/whiner_tools.rb
+++
@@ -1,8 +0,0 @@
-# ruby ~ whiner_tools.rb
-
-module WhinerTools
-  VERSION = "0.0.1" unless defined?(VERSION)
-end
-
-require File.dirname(__FILE__)+'/whiner_tools/logger'
-require File.dirname(__FILE__)+'/whiner_tools/global'
